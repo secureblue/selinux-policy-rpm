47948f528c98df6cd2866946bc8c505db20ec41a
Lukas Vrabec <lvrabec@redhat.com>
2017-02-15 15:34:56 +0100

    Update make-rhat-patches.sh script to download patches from selinux-policy-contrib repo

--- make-rhat-patches.sh
+++ make-rhat-patches.sh
@@ -12,12 +12,17 @@ POLICYSOURCES=`mktemp -d policysources.XXXXXX`
 pushd $POLICYSOURCES > /dev/null
 
 git clone git@github.com:fedora-selinux/selinux-policy.git -q
+git clone git@github.com:fedora-selinux/selinux-policy-contrib.git -q
 git clone git@github.com:projectatomic/container-selinux.git -q
 
 pushd selinux-policy > /dev/null
 # prepare policy patches against upstream commits matching the last upstream merge
 git rev-parse --verify origin/${FEDORA_VERSION}-base; git diff eb4512f6eb13792c76ff8d3e6f2df3a7155db577 origin/${FEDORA_VERSION}-base > policy-${FEDORA_VERSION}-base.patch
-git rev-parse --verify origin/${FEDORA_VERSION}-contrib; git diff 64302b790bf2b39d93610e1452c8361d56966ae0 origin/${FEDORA_VERSION}-contrib > policy-${FEDORA_VERSION}-contrib.patch
+popd > /dev/null
+
+pushd selinux-policy-contrib > /dev/null
+# prepare policy patches against upstream commits matching the last upstream merge
+git rev-parse --verify origin/${FEDORA_VERSION}; git diff 64302b790bf2b39d93610e1452c8361d56966ae0 origin/${FEDORA_VERSION} > policy-${FEDORA_VERSION}-contrib.patch
 popd > /dev/null
 
 pushd container-selinux > /dev/null
@@ -27,7 +32,8 @@ tar -czf container-selinux.tgz container.if container.te container.fc
 popd > /dev/null
 
 pushd $DISTGIT_PATH > /dev/null
-cp $POLICYSOURCES/selinux-policy/policy-${FEDORA_VERSION}-{base,contrib}.patch .
+cp $POLICYSOURCES/selinux-policy/policy-${FEDORA_VERSION}-base.patch .
+cp $POLICYSOURCES/selinux-policy-contrib/policy-${FEDORA_VERSION}-contrib.patch .
 cp $POLICYSOURCES/container-selinux/container-selinux.tgz .
 popd > /dev/null
 
