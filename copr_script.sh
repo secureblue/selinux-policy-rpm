0b44551691a1c561fa994158ba20ba6b1289f09b
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-10-05 20:22:50 -0700

    comments

--- copr_script.sh
+++ copr_script.sh
@@ -1,14 +1,16 @@
-#! /bin/sh -x 
+#!/bin/sh -eux
 
 LATEST_COMMIT=$(git ls-remote https://github.com/secureblue/selinux-policy.git refs/heads/f42-secureblue | awk '{print $1}')
-SECUREBLUE_REPO_URL=$(https://github.com/secureblue/selinux-policy.git)
+SECUREBLUE_REPO_URL='https://github.com/secureblue/selinux-policy.git'
 
-git clone https://src.fedoraproject.org/rpms/selinux-policy.git
+git clone --depth=1 https://src.fedoraproject.org/rpms/selinux-policy.git
 cd ./selinux-policy
 git checkout f42
-sed -i "s/^%global giturl .*/%global giturl $SECUREBLUE_REPO_URL/" selinux-policy.spec
-sed -i "s/^%global commit .*/%global commit $LATEST_COMMIT/" selinux-policy.spec
-sed 's/^Version: .*/&-secureblue/' selinux-policy.spec
+sed --sandbox -i \
+  -e "s|^%global giturl .*|%global giturl $SECUREBLUE_REPO_URL|" \
+  -e "s/^%global commit .*/%global commit $LATEST_COMMIT/" \
+  -e '/^Version: /s/$/-secureblue/' \
+  selinux-policy.spec
 cd ..
 
 mv ./selinux-policy/* .
