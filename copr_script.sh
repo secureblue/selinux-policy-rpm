1d52dad533b60e7e5a7b5b994c594c791f77023c
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-10-07 15:14:28 -0700

    fix(build): drop --depth=1 (#4)

--- copr_script.sh
+++ copr_script.sh
@@ -3,7 +3,7 @@
 LATEST_COMMIT=$(git ls-remote https://github.com/secureblue/selinux-policy.git refs/heads/f42-secureblue | awk '{print $1}')
 SECUREBLUE_REPO_URL='https://github.com/secureblue/selinux-policy'
 
-git clone --depth=1 https://src.fedoraproject.org/rpms/selinux-policy.git
+git clone https://src.fedoraproject.org/rpms/selinux-policy.git
 cd ./selinux-policy
 git checkout f42
 sed --sandbox -i \
