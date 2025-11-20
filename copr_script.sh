d560381502b74769543fd68ad18c514c78b74f45
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-11-20 14:08:35 -0800

    feat: add cil handling to the spec (#8)

--- copr_script.sh
+++ copr_script.sh
@@ -11,6 +11,7 @@ sed --sandbox -i \
   -e "s/^%global commit .*/%global commit $LATEST_COMMIT/" \
   -e '/^Version: /s/$/_secureblue/' \
   selinux-policy.spec
+sed -i -e '/^make clean$/a semodule -v -i ./cil/*.cil' selinux-policy.spec
 cd ..
 
 mv ./selinux-policy/* .
